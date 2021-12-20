d0658772ff06bed4164d5a3d9dea31477feb3a38
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-12-19 20:51:20 -0500

    added logo and centered section

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import { Row, Col } from "react-bootstrap";
 import pic from "../assets/headshot.jpg";
 import "./Content.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Content = () => {
   return (
@@ -17,7 +19,7 @@ const Content = () => {
         </Col>
         <Col className="bg-gradient-primary rounded">
           <h1>Welcome!</h1>
-          <p>
+          <p className="">
             My name is Thomas Levine, and I love to learn how things work. I
             earned a Bachelor of Arts in Music from Virginia Commonwealth
             University with a Minor in Computer Science.
@@ -28,7 +30,10 @@ const Content = () => {
       <hr></hr>
       <Row>
         <Col d-flex justify-content-center>
-          <h4>Lets connect!</h4>
+          <h4 className="text-center">Lets connect!</h4>
+          <div className="d-flex justify-content-center mt-2">
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
+          </div>
         </Col>
       </Row>
     </Container>
