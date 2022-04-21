c8cad6abba66735dfc538fbb7e32daf46c57c243
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2022-04-21 18:12:36 -0400

    added additional content

--- src/components/Content.js
+++ src/components/Content.js
@@ -26,6 +26,45 @@ const Content = () => {
           <br></br>
         </Col>
       </Row>
+      <Row id="about" className="w-75 mx-auto">
+        <h1>About me</h1>
+        <p>
+          I am currently a Programmer Analyst at CGI Federal, where I work to support
+          the Department of State Joint Financial Management System. I work intesively with
+          distributed systems, workload management software such as BMC Control-M, Oracle Products
+          such as J2EE, WebLogic, Oracle SQL (PL/SQL), and shell scripting in CMD and Bash. While working
+          here, I have gained a deeper understanding of the paradigms and architectures that underpin
+          major enterprise applications. I have also been deeply ingrained in the Agile Methodology, and
+          the Testing and Maintenance phases of the Software Development Life Cycle.
+        </p>
+        <p>
+          While at CGI, I have also spent my time developing an internal full stack application
+          titled the <em>Devnet Info Page</em> which is a refreshed version of a .NET application. 
+          The website services nearly 100 members, and is designed to provide environment and server
+          information within out internal networks, allowing real-time updates on services and providing
+          a convenient location to store all relevant information. We used Angular JS, Express, Node, and Oracle
+          DB for the application stack, which was in itself an obstacle to create new organizational and functional
+          patterns from the old architecture. I greatly enjoy working on projects that directly service individuals, and
+          where I can observe the impact on others workflows.
+        </p>
+        <p>
+          Outside of work, I like to work on projects that connect to my interests and also enhance my skills. I recently
+          worked on an application named <a href="www.tiktokvralchecker.com"><em>Tiktok Viral Checker</em></a> which I 
+          built to check if a given song is viral on Tiktok, since I don't use Tiktok myself. I also love to make Music
+          and see live music. I play 5 instruments, but most regularly play guitar.
+        </p>
+      </Row>
+      <Row id="projects" className="w-75 mx-auto">
+        <h1>Projects</h1>
+        <div>
+          <h3>Tiktok Viral Checker</h3>
+        </div>
+        <h3>Fantasy Football Twitter Bot</h3>
+        <h3>Discord Dinosaur Bot</h3>
+        <Col className="text-center">a</Col>
+        <Col className="text-center">b</Col>
+        <Col className="text-center">C</Col>
+      </Row>
       <hr></hr>
       <Row>
         <Col d-flex justify-content-center>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div class="d-flex main bg-dark text-light">
-            Testing Footer
+        <div class="d-flex main p-2 bg-dark text-light rounded-top">
+            <h3>Contact</h3>
+            <h5>🅿️</h5>
         </div>
     )
 }
